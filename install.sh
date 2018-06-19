3929d620efe9dae426e8482fb077c8557865dec9
Adam Elnagger <adam.elnagger@gmail.com>
2018-06-18 22:02:27 -0400

    Another iteration on the installer.

--- install.sh
+++ install.sh
@@ -12,22 +12,27 @@ then
   sudo chown -R root:root arm
   cd arm
 fi
-sudo su && \
-add-apt-repository -y ppa:heyarje/makemkv-beta && \
-add-apt-repository -y ppa:stebbins/handbrake-releases && \
-add-apt-repository -y ppa:mc3man/xerus-media && \
-apt update && \
-apt install -y makemkv-bin makemkv-oss && \
-apt install -y handbrake-cli libavcodec-extra && \
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'" && \
-apt install -y abcde flac imagemagick glyrc cdparanoia && \
-apt install -y at && \
-apt install -y python3 python3-pip && \
-apt-get install -y libdvd-pkg && \
-dpkg-reconfigure libdvd-pkg && \
-su && \
-pip3 install -r requirements.txt && \
-ln -s /opt/arm/51-automedia.rules /lib/udev/rules.d/ && \
-ln -s /opt/arm/.abcde.conf /root/ && \
-cp /opt/arm/arm@.service /etc/systemd/system/ && \
+
+echo Installing dependencies
+export DEBIAN_FRONTEND=noninteractive && \
+sudo add-apt-repository -y ppa:heyarje/makemkv-beta && \
+sudo add-apt-repository -y ppa:stebbins/handbrake-releases && \
+sudo add-apt-repository -y ppa:mc3man/xerus-media && \
+sudo apt update && \
+sudo apt install -y makemkv-bin makemkv-oss && \
+sudo apt install -y handbrake-cli libavcodec-extra && \
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'" && \
+sudo apt install -y abcde flac imagemagick glyrc cdparanoia && \
+sudo apt install -y at && \
+sudo apt install -y python3 python3-pip && \
+sudo apt-get install -y libdvd-pkg && \
+sudo dpkg-reconfigure libdvd-pkg && \
+sudo pip3 install -r requirements.txt
+
+echo Installing rules ad services
+sudo ln -s /opt/arm/51-automedia.rules /lib/udev/rules.d/ && \
+sudo ln -s /opt/arm/.abcde.conf /root/ && \
+sudo cp /opt/arm/arm@.service /etc/systemd/system/
+
+echo Copying sample config
 cp config.sample config
